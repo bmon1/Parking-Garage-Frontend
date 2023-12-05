a76ad6c58ebc43abd4fb868c5f78d1abb13af15d
Branden Monroe <brandenmonroe1@yahoo.com>
2023-12-05 15:11:04 -0600

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1>Parking Garage Management</h1>
+
+  </main>
+</template>
+
+
+<script>
+
+import axios from 'axios';
+export default {
+  name: 'names',
+  data() {
+    return {
+      name: []
+    }
+  },
+  mounted() {
+    this.getName();
+  },
+  methods: {
+    async getName() {
+      await axios.get('http://localhost:80/api/names').then(res => {
+        console.log(res);
+      })
+    },
+  },
+}
+</script>
---
+++ src/views/Names/Name.vue
@@ -0,0 +1,27 @@
+
+<template>
+   <h1>Name</h1>
+</template>
+
+<script>
+
+import axios from 'axios';
+export default {
+   name: 'names',
+   data() {
+      return {
+         name: []
+      }
+   },
+   mounted() {
+      this.getName();
+   },
+   method: {
+      async getName() {
+         await axios.get('http://localhost:80/api/names').then(res => {
+            console.log(res);
+         })
+      }
+   },
+}
+</script>
