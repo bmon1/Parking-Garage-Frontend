4513ea162189d1b8ce43b4c9e5c736883ed20165
Branden Monroe <brandenmonroe1@yahoo.com>
2023-12-07 16:51:18 -0600

    checks all 3 index methods correct response

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,12 +21,29 @@ export default {
     }
   },
   mounted() {
-    this.getName();
+    this.getUsers();
+    this.getVehicles();
+    this.getGarages();
   },
   methods: {
     async getName() {
       await axios.get('http://localhost:80/api/names').then(res => {
         console.log(res);
+      })
+    },
+    async getVehicles() {
+      await axios.get('http://localhost:80/api/vehicles').then(res => {
+        console.log(res);
+      })
+    },
+    async getUsers() {
+      await axios.get('http://localhost:80/api/users').then(response => {
+        console.log(response);
+      })
+    },
+    async getGarages() {
+      await axios.get('http://localhost:80/api/garages').then(response => {
+        console.log(response);
       })
     },
   },
